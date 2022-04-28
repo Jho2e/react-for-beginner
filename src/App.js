fe7e7902d3d67537b539c8bae9912721e27ac3e5
Jho2e <ohjaehokor@naver.com>
2022-04-28 10:25:02 +0900

    useState - CleanUp 기능
    useEffect에 return을 주어서 실행

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
+import { func } from "prop-types";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
+function Hello() {
   useEffect(() => {
-    console.log("only once");
+    console.log("hi :)");
+    return () => {
+      console.log("bye :(");
+    };
   }, []);
+  return <h1>Hello</h1>;
+}
 
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 3) {
-      console.log("search for", keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("click change");
-  }, [counter]);
-  useEffect(() => {
-    console.log("or");
-  }, [counter, keyword]);
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
